d5288fc97db0063c19571ab28f8ec78dd776d16d
dayfundora <dayfundoraglez@gmail.com>
2021-05-13 13:51:25 -0400

    minimax alphabeta

--- minimax.py
+++ minimax.py
@@ -12,40 +12,50 @@ def minimax(game, player, depth, h, moves):
     best, value = maxplay(game, None, player, depth, h, moves)
     return best
 
-def maxplay(game, play, player, depth, h, moves):
+def maxplay(game, play, player, depth, h, moves, alpha=-oo , beta=oo):
     """Retorna la mejor jugada tablero para el jugador"""
     best = None
     best_value = -oo
 
     if game.winner() != EMPTY:
-        return play, 1 if game.winner() == player else -1
+        return play, -1
+        #return play, 1 if game.winner() == player else -1
 
     if not depth:
         return play, h(game, player)
 
     for x,y in moves(game, player):
         b, value = minplay(game.clone_play(x,y), (x,y), player, depth - 1, h, moves)
+        alpha = max(alpha, value)
+
+        if alpha >= beta:
+            break
 
         if value > best_value:
             best = (x,y)
             best_value = value
-
+        
     return best, best_value
 
-def minplay(game, play, player, depth, h, moves):
+def minplay(game, play, player, depth, h, moves, alpha=-oo , beta=oo):
     """Retorna la mejor jugada para el jugador contrario"""
     best = None
     best_value = oo
 
     if game.winner() != EMPTY:
-        return play, 1 if game.winner() != player else 0
+        return play, 1
+        #return play, 1 if game.winner() == player else -1
 
     if not depth:
         return play, h(game, player)
 
     for x,y in moves(game, player):
         _, value = maxplay(game.clone_play(x,y), (x,y), player, depth - 1, h, moves)
+        beta = min(beta, value)
 
+        if beta <= alpha:
+            break
+    
         if value < best_value:
             best = (x,y)
             best_value = value
